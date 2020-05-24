1c0af219c7a47b7bd3623e92d313deab3864c82c
Raunaq Singh <raunaqsingh777@gmail.com>
2020-05-24 17:45:51 +0530

    Added Post mapping with RequestParam

--- src/main/java/com/intuit/developer/helloworld/controller/SlackController.java
+++ src/main/java/com/intuit/developer/helloworld/controller/SlackController.java
@@ -1,14 +1,16 @@
 package com.intuit.developer.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SlackController {
     @ResponseBody
-    @RequestMapping("/slack/events")
-    public String slashCommandResponse(){
-        return "Success";
+    @PostMapping("/slack/events")
+    public String slashCommandResponse(@RequestParam("text") String text){
+        return text;
     }
 }
