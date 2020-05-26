26b6a0a92cd77b2bc47bb3a7ad8eb6767c841b05
Raunaq Singh <raunaqsingh777@gmail.com>
2020-05-27 01:32:18 +0530

    Removing the comments

--- src/main/java/com/intuit/developer/helloworld/controller/SlackController.java
+++ src/main/java/com/intuit/developer/helloworld/controller/SlackController.java
@@ -71,8 +71,7 @@ public class SlackController {
         logger.info("+++++++++" + responseURL);
         restTemplate.postForEntity(responseURL, new HttpEntity<>(slackResponse, getHeaders()), slackResponse.getClass());
         logger.info("+++++++++ REACHED HERE");
-        return "Checking";
-        /*
+        //return "Checking";
         try {
             // get DataService
             final DataService service = helper.getDataService(credentials.getRealmID(), credentials.getAccessToken());
@@ -110,7 +109,6 @@ public class SlackController {
             list.forEach(error -> logger.error("Error while calling the API :: " + error.getMessage()));
             return new JSONObject().put("response", "Failed").toString();
         }
-        */
     }
 
     private Customer getCustomerWithAllFields() {
