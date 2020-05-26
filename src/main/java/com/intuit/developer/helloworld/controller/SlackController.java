a30947037789812ac4f8a200841b4a8570345157
Raunaq Singh <raunaqsingh777@gmail.com>
2020-05-27 01:15:53 +0530

    Removed ResponseBOdy

--- src/main/java/com/intuit/developer/helloworld/controller/SlackController.java
+++ src/main/java/com/intuit/developer/helloworld/controller/SlackController.java
@@ -33,6 +33,7 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
